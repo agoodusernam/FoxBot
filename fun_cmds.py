97513efe14ab50a5627796b1905fba0086b2f6d5
Hardline <hardlinemouse16@gmail.com>
2025-06-02 22:09:04 +0200

    im dum

--- fun_cmds.py
+++ fun_cmds.py
@@ -12,11 +12,11 @@ async def dice_roll(client, message: discord.Message) -> None:
 		await message.channel.send("Please choose 2 numbers to roll the dice, e.g. `dice 1 6`", delete_after=client.del_after)
 		return
 	nums = list(map(int, nums))
-	num: int | str = random.randint(nums[0], nums[1])
+	num: int | bytes = random.randint(nums[0], nums[1])
 	oversize = False
 	if math.log10(num) > 1900:
 		oversize = True
-		num = base64.b64encode(str(num).encode()).decode()
+		num = str(num).encode()
 
 	await message.channel.send(f"You rolled a {num}")
 	if oversize:
