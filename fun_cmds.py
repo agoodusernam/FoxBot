5ee6be483087e4fb300b76f5d091c3b93ff732f3
agoodusernam <jamiekoehring.jk@gmail.com>
2025-06-02 13:33:08 +0200

    Just let me type annotate PLEASE

--- fun_cmds.py
+++ fun_cmds.py
@@ -1,10 +1,8 @@
 import discord
 import random
 
-import main
 
-
-async def dice_roll(client: main.MyClient, message: discord.Message) -> None:
+async def dice_roll(client, message: discord.Message) -> None:
 	nums: list[int | str] = message.content.replace("dice", "").replace("roll", "").split()
 	if len(nums) < 2:
 		await message.channel.send("Please choose 2 numbers to roll the dice, e.g. `dice 1 6`", delete_after=client.del_after)
