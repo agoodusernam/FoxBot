d91a4708868975cd31fcc2c1b73ea7f2fc29ec5c
agoodusernam <jamiekoehring.jk@gmail.com>
2025-08-01 09:27:17 +0200

    im dum

--- cogs/currency_cmds.py
+++ cogs/currency_cmds.py
@@ -637,7 +637,7 @@ class CurrencyCmds(commands.Cog, name='Currency', command_attrs=dict(add_check=i
                 # If we found qualified jobs at this level, add them and break
                 if qualified_jobs:
                     for job in qualified_jobs:
-                        if job.name() == profile['work_str'] or job.tree.name == profile['work_tree']:
+                        if job.tree.name == profile['work_tree']:
                             continue
                         # Calculate the salary with variance to 0.01% precision
                         salary_mult = 1 + (random.randint(0, job.salary_variance * 10) / 1000) # salary variance is an int for percentage
