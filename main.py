fda1eec8ed11ddee5c190053272c974c3f7f6fe9
agoodusernam <90071032+agoodusernam@users.noreply.github.com>
2026-01-15 14:12:56 +0100

    asyncio is hard

--- main.py
+++ main.py
@@ -141,13 +141,9 @@ async def on_message(message: discord.Message):
     _ = message
 
 # Run the bot
-async def main():
-    token = os.getenv('TOKEN')
-    if not isinstance(token, str):
-        raise TypeError('TOKEN environment variable not set.')
-    
-    bot.help_command = help_cmd.CustomHelpCommand()
-    bot.run(token=token, reconnect=True, log_handler=None)
+token = os.getenv('TOKEN')
+if not isinstance(token, str):
+    raise TypeError('TOKEN environment variable not set.')
 
-if __name__ == '__main__':
-    asyncio.run(main())
+bot.help_command = help_cmd.CustomHelpCommand()
+bot.run(token=token, reconnect=True, log_handler=None)
